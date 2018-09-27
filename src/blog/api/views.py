0ad3da35bf702d26e185b01fc00cdb7c680f7a75
Rick <654834341@qq.com>
2018-09-27 13:52:15 -0500

    test

--- src/blog/api/views.py
+++ src/blog/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from blog.models import Blog
 from blog.api.paginations import BlogPagination
 
+
 class BlogViewSet(viewsets.ModelViewSet):
     queryset = Blog.objects.all()
     pagination_class = BlogPagination
-    
