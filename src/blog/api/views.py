8019a304d3cfd2caf00be4f75e20c44ae66aff3d
Rick <654834341@qq.com>
2018-09-30 19:53:27 -0500

    Fix bug

--- src/blog/api/views.py
+++ src/blog/api/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
 from blog.models import Blog
 from blog.api.paginations import BlogPagination
+from serializers import BlogSerializer
 
 
 class BlogViewSet(viewsets.ModelViewSet):
     queryset = Blog.objects.all()
     pagination_class = BlogPagination
+    serializer_class = BlogSerializer
